5582dd7e95b86028ea91f68fb0586d3148c1c8c9
Abhas Kumar <abhasroy@gmail.com>
2020-05-10 21:08:35 -0700

    Changed Animation using POD file

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,22 +7,15 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = ""
-        let title = "⚡️FlashChat"
-        var indexofTitle: Double = 0
-        for eachLetterInTitle in title {
-            indexofTitle += 1
-            Timer.scheduledTimer(withTimeInterval: 0.1 * indexofTitle, repeats: false) { (timer) in
-                self.titleLabel.text?.append(eachLetterInTitle)
-            }
-        }
+        titleLabel.text = "⚡️FlashChat"
         
     }
 }
